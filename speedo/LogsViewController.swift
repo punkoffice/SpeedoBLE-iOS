25d3c6b5a10e6a20a469beb12d258e4e7f5e02cb
Marcus Milne <marcusmilne@punkoffice.com>
2021-09-13 00:01:26 +1000

    Show logs in log table

--- speedo/LogsViewController.swift
+++ speedo/LogsViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Marcus Milne on 12/9/21.
 //
 
+import CoreData
 import UIKit
 
 class LogsViewController: UITableViewController {
@@ -23,23 +24,35 @@ class LogsViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Global.DBitemsLogs!.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let DBMOlog = Global.DBitemsLogs![indexPath.row] as? NSManagedObject
+        let datetime = DBMOlog!.value(forKey: "datetime") as! Date
+        let topSpeed = DBMOlog!.value(forKey: "topSpeed") as! Int
+        let distance = DBMOlog!.value(forKey: "distance") as! Int
+        let duration = DBMOlog!.value(forKey: "duration") as! Int
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuse", for: indexPath)
+        let lblDateTime = cell.viewWithTag(1) as? UILabel
+        let lblTopSpeed = cell.viewWithTag(2) as? UILabel
+        let lblDistance = cell.viewWithTag(3) as? UILabel
+        let lblDuration = cell.viewWithTag(4) as? UILabel
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        let strDateTime = dateFormatter.string(from: datetime)
+        lblDateTime?.text = strDateTime
+        lblTopSpeed?.text = topSpeed.description
+        let dblDistance = Double(distance)/1000.0
+        lblDistance?.text =  String(format: "%.1f", dblDistance)
+        lblDuration?.text = duration.description
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
